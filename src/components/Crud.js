ff235fe5b44dd6ca6df1a5066882171ee7e7525c
Mikael Svensson <113359307+mikeCre8@users.noreply.github.com>
2023-04-12 14:23:03 +0200

    added shadow to table

--- src/components/Crud.js
+++ src/components/Crud.js
@@ -174,7 +174,7 @@ const Crud = () => {
     return (
       <thead>
         <tr>
-          <th colSpan="4" className="table-dark rounded-top">
+          <th colSpan="4" className="table-dark rounded-top shadow">
             <div className="d-flex justify-content-between align-items-center">
               <div>Person Lista <button type="button" className='btn btn-primary' onClick={togleSearch}>Sök</button></div>
             </div>
