4a6746b6c4798b595f6eeefccb66fd4ac77c2acc
Mikael Svensson <113359307+mikeCre8@users.noreply.github.com>
2023-05-02 08:56:16 +0200

    changes to dokument

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -7,18 +7,11 @@ const Documents = () => {
 
          {/* Card */}
          <div className="card bg-dark shadow text-center" style={{ width: "15rem" }}>
-            {/* Header */}
-            <div className="card-header">
-               <h5 className="card-title">Dokument 1</h5>
-            </div>
-            {/* Body */}
+
             <div className="card-body">
-               <a href="./Image/Big-Sur-Shore-Rocks.png">
-                  <img src="./Image/Big-Sur-Shore-Rocks.png" target="_blank" alt="document_img" className='rounded shadow' style={{ width: "12rem" }} />
+               <a href="#">                           {/* Remember to change the "#" */}
+                  <h5 className="card-title text-info">Öppna PDF</h5>
                </a>
-            </div>
-            {/* Footer */}
-            <div className="card-footer">
                <a href="#" download>Ladda ner</a>     {/* Remember to change the "#" */}
             </div>
          </div>
