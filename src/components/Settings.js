86d180056a5ed796fe872e29f5ca7763ec11b295
Mikael Svensson <113359307+mikeCre8@users.noreply.github.com>
2023-06-09 10:35:38 +0200

    some styling on settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -13,11 +13,12 @@ const Settings = () => {
     console.log(user);
     const dispatch = useDispatch();
     const [showEdit, setShowEdit] = useState(false);
-    
+
     const handellogout = () => {
         window.location.href = "http://localhost:3000/login"
         setTimeout(() => {
-        dispatch(logout());}, 2000);
+            dispatch(logout());
+        }, 2000);
     }
 
     const handeleditbtn = () => {
@@ -26,22 +27,24 @@ const Settings = () => {
     const editSubmit = () => {
         // send request about to change the profile pic 
     }
- 
-   
+
+
     return (
 
         <div className='container'>
-        <h4 className='text-center'>Inställningar</h4>
-        <div className="profile_pic_container">
-          <Avatar sx={{ bgcolor: green }}>{user.firstName.charAt(0).toUpperCase() + user.lastName.charAt(0).toUpperCase()}</Avatar>
+            <h4 className='text-center'>Inställningar</h4>
+            <div className="profile_pic_container shadow">
+                <Avatar sx={{ bgcolor: green }}>{user.firstName.charAt(0).toUpperCase() + user.lastName.charAt(0).toUpperCase()}</Avatar>
+            </div>
+            <br />
+            <div className="container card-body bg-light p-3 rounded shadow">
+                <h1>{user.firstName} {user.lastName}</h1>
+                <h3>{user.role.roleTitle}</h3>
+                <h6>ID: {user.id} </h6>
+                <div className="mb-3">change password (send a request, user get an email and then change the password) ? <br /> change email (only admin can do if user needs to) ?</div>
+                <button type="button" className="btn btn-danger" onClick={handellogout} >logout</button>
+            </div>
         </div>
-        <br />
-        <h1>{user.firstName} {user.lastName}</h1>
-        <h3>{user.role.roleTitle}</h3>
-        <h6># {user.id} </h6>
-        <div className="mb-3">change password (send a request, user get an email and then change the password) ? <br /> change email (only admin can do if user needs to) ?</div>
-        <button type="button" className="btn btn-danger" onClick={handellogout} >logout</button>
-      </div>
 
     );
 
